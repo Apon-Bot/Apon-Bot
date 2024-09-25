5470de5d9bd43a0f4f11f4526f8f6a21acd371d6
Apon Islam <a4p9n0k@gmail.com>
2024-09-25 17:43:47 +0600

    Update ilove.js

--- scripts/commands/ilove.js
+++ scripts/commands/ilove.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports.config = {
-  name: "iloveu",
+  name: "apon",
   version: "2.0.0",
   permission: 0,
   credits: "nayan",
@@ -13,9 +13,9 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("I love you")==0 || (event.body.indexOf("i love you")==0 || (event.body.indexOf("I love u")==0 || (event.body.indexOf("I love you")==0)))) {
+	if (event.body.indexOf("apon")==0 || (event.body.indexOf("Apon")==0 || (event.body.indexOf("apon")==0 || (event.body.indexOf("Apon")==0)))) {
 		var msg = {
-				body: "Hmm... বস নয়নও তোমাকে ভালোবাসে😇😻 :))"
+				body: "জ্বী আমায় বলুন আপন বস বিজি আছে পরে কথা বলবে💚💚 <3"
     }
 			api.sendMessage(msg, threadID, messageID);
 		}
